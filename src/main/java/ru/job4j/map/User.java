6c498f8d5f9cae79e58cefa69a4017c75ca9ab4e
Azamat-Sult <asultangareev@gmail.com>
2021-05-14 23:48:44 +0500

    User.java update

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -35,5 +35,29 @@ public class User {
 
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Bob", 2, new GregorianCalendar());
+        User user2 = new User("Bob", 2, new GregorianCalendar());
+        System.out.println("user1.hashCode() = " + user1.hashCode());
+        System.out.println("user2.hashCode() = " + user2.hashCode());
+        Map<User, Object> bobsMap = new HashMap<>();
+        bobsMap.put(user1, new Object());
+        bobsMap.put(user2, new Object());
+        System.out.println(bobsMap);
     }
 }
