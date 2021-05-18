1e9a992de824b7ffcc3b2ab52e243cc97496d029
Azamat-Sult <asultangareev@gmail.com>
2021-05-18 22:48:50 +0500

    104. Maximum Depth of Binary Tree

---
+++ src/test/java/ru/job4j/leetcode/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.leetcode;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MaximumDepthOfBinaryTreeTest {
+
+    @Test
+    public void test1() {
+        MaximumDepthOfBinaryTree tree = new MaximumDepthOfBinaryTree();
+        MaximumDepthOfBinaryTree.TreeNode node15 = new MaximumDepthOfBinaryTree.TreeNode(15,null,null);
+        MaximumDepthOfBinaryTree.TreeNode node7 = new MaximumDepthOfBinaryTree.TreeNode(7,null,null);
+        MaximumDepthOfBinaryTree.TreeNode node20 = new MaximumDepthOfBinaryTree.TreeNode(20, node15, node7);
+        MaximumDepthOfBinaryTree.TreeNode node9 = new MaximumDepthOfBinaryTree.TreeNode(9,null,null);
+        MaximumDepthOfBinaryTree.TreeNode root3 = new MaximumDepthOfBinaryTree.TreeNode(3, node9, node20);
+        assertThat(tree.maxDepth(root3), is(3));
+    }
+
+}
