94ab2a641ddd69022cd5bc9b45d27aeeba014da9
Azamat-Sult <asultangareev@gmail.com>
2021-06-04 20:33:16 +0500

    0. Что такое Socket? [#4850 #163514]. Исправление 1

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,14 +3,9 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -20,31 +15,22 @@ public class EchoServer {
                     String str;
                     String request = "";
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.startsWith("GET /?")) {
-                            String[] lineSplit = str.split(" ");
+                        String[] lineSplit = str.split(" ");
+                        if (lineSplit[1].startsWith("/?msg")) {
                             String[] requestSplit = lineSplit[1].split("=");
                             request = requestSplit[1];
                         }
                         System.out.println(str);
                     }
-                    System.out.println("---------------------------------------------");
-                    String answerToClient = request;
-                    if (Objects.equals(request, "Exit")) {
-                        answerToClient = "SERVER is SHUTDOWN";
+                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    if (request.equals("Bye")) {
+                        out.write("SERVER is SHUTDOWN\r\n".getBytes());
                         server.close();
                     }
-                    if (Objects.equals(request, "Hello")) {
-                        answerToClient = "Hello, dear friend!";
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write(answerToClient.getBytes());
-                } catch (IOException e) {
-                    LOG.error("EXCEPTION: ", e);
+                    System.out.println("-----------------------------------------");
                 }
             }
             System.out.println("SERVER is SHUTDOWN");
-        } catch (IOException e) {
-            LOG.error("EXCEPTION: ", e);
         }
     }
 }
