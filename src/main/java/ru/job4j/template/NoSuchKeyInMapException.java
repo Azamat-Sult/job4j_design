c420c93f56561048c039cf82793c3f582ff54770
Azamat-Sult <asultangareev@gmail.com>
2021-07-29 17:26:21 +0500

    3. Шаблонизатор. [#855]

---
+++ src/main/java/ru/job4j/template/NoSuchKeyInMapException.java
@@ -0,0 +1,4 @@
+package ru.job4j.template;
+
+public class NoSuchKeyInMapException extends Exception {
+}
---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorTest {
+
+    @Test(expected = NoSuchKeyInMapException.class)
+    public void whenGenerateThenKeyNotFoundInMap() throws NoSuchKeyInMapException, NoSuchKeyInTemplateException {
+        Generator generator = new StringGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Jerry");
+        String rsl = generator.produce("${age}, ${address}", map);
+    }
+
+    @Test(expected = NoSuchKeyInTemplateException.class)
+    public void whenGenerateThenKeyNotFoundInTemplate() throws NoSuchKeyInTemplateException, NoSuchKeyInMapException {
+        Generator generator = new StringGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Jerry");
+        map.put("age", "21");
+        String rsl = generator.produce("${age}", map);
+    }
+}
